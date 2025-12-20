47e00b37ab4c8c86e5dedd5ede860d6e43bf7a1e
AlgoleTejaswi15 <23951a05br@iare.ac.in>
2025-12-20 15:40:03 +0530

    Time: 0 ms (100%), Space: 17.3 MB (99.75%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq=[0]*101
+        count=0
+        for num in nums:
+            count+=freq[num]
+            freq[num]+=1
+        return count
+
+
+
+        
